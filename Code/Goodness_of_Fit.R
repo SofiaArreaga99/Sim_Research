ad769b93b60daed8254ea95965f6e47d458a3349
SofiaArreaga99 <sigreduccion@gmail.com>
2025-09-04 13:57:44 -0400

    Add project organization to directory

--- Code/Goodness_of_Fit.R
+++ Code/Goodness_of_Fit.R
@@ -7,13 +7,36 @@
 #Ho= Equal distribution of "correct choices"
 #Ha= Not equal distribution 
 
-#We are taking a sample of n= 100 of the whole population 
+# Observed vs. Theoretical Data
+
+# We are taking a sample of n= 100 of the whole population 
 
 observed = c(20,20,25,35)
 theo = c(1/4,1/4,1/4,1/4) #This value has to be in proportion
 
 chisq.test (x=observed,
             p=theo)
+
+# Two by two table  
+
+# Crear tabla 2x2 con un tema de naturaleza
+data <- matrix(c(25, 15,   # Con humedal: 25 con aves, 15 sin aves
+                 8,  32),  # Sin humedal: 8 con aves, 32 sin aves
+               nrow = 2,
+               byrow = TRUE)
+
+colnames(data) <- c("Aves_presentes", "Aves_ausentes")
+rownames(data) <- c("Con_humedal", "Sin_humedal")
+
+tabla <- as.table(data)
+tabla
+
+chisq.test(tabla, correct = TRUE)
+
+
+
+
+
 
 
 
